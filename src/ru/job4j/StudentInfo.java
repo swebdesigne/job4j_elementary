81bbae92019e8788595d7e30342c7925e8234850
Igor <swebdesigne@gmail.com>
2020-11-29 16:45:10 +0300

    3.3. Разбор первой программы. [#420293]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Igor Sivolobov");
+        System.out.println("12.11.1987");
+    }
+}
