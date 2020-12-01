3f0d5fdbeefa633ba46f3fb06267f30982606426
Igor <swebdesigne@gmail.com>
2020-12-01 16:32:03 +0300

    Определите тип данных для переменных. [#420277]

---
+++ src/ru/job4j/calculate.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+public class calculate {
+    public static void main(String[] args){
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int length = 100500;
+        System.out.print(length);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+        byte age = 127;
+        System.out.println(age);
+        float length_1  = 8.0332F;
+        System.out.println(length_1);
+        boolean baby = true;
+        System.out.println(baby);
+        double max = 1.7e+308;
+        System.out.println(max);
+    }
+}
