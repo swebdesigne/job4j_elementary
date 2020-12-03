9e19b8f369a16fff1adb0d3cc9a0a89d5ee8555f
swebdesigne <swebdesigne@gmail.com>
2020-12-03 19:55:03 +0300

    String

---
+++ src/ru/job4j/calculator/Out.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+public class Out {
+    public static void main(String[] args){
+       String name = "Igor";
+       byte age = 33;
+       String separate = " - ";
+       String id = name + separate + age + " years old";
+       int first = 1;
+       int second = 2;
+       int onePlusTwo = first + second;
+       System.out.println("1+2 = " + onePlusTwo);
+       System.out.println(id);
+    }
+}
---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args){
+        short year = 2020;
+        String idea = "I like java! " + year;
+        System.out.println(idea + " But I am a newbie.");
+    }
+}
