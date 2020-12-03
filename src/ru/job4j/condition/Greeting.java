862e446a9f1ca480e6f7f1ec56226b9664cc24ab
swebdesigne <swebdesigne@gmail.com>
2020-12-03 20:00:10 +0300

    String

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args){
         short year = 2020;
-        String idea = "I like java! " + year;
-        System.out.println(idea + " But I am a newbie.");
+        String idea = "I like java! ";
+        System.out.println(idea + year + " But I am a newbie.");
     }
 }
