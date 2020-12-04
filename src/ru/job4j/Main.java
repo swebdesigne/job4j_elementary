92020276f355a1826c00f61aeced4cf3d731d57d
Igor <swebdesigne@gmail.com>
2020-12-04 11:19:07 +0300

    Checkstyle

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class Main {
     @SuppressWarnings("checkstyle:NoWhitespaceAfter")
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 }
