c29cef521de2238f0769657b52382e75996834f5
Igor <swebdesigne@gmail.com>
2020-12-04 11:02:13 +0300

    Checkstyle

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -1,10 +1,9 @@
 package ru.job4j.condition;
 
 public class TrgArea {
-    public static double area(double a, double b, double c) {
-        double p = (a+b+c)/2;
-        double rsl = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-        return rsl;
+    public static double area(double a, double b, double c){
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
