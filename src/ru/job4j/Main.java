c8d4560b1a3f5b8b00a9fba1e803a0bb3b116a19
Igor <swebdesigne@gmail.com>
2020-12-07 09:23:23 +0300

    Оператор if с блоком else

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -3,8 +3,8 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world ");
-        int twentyFife = 10000;
-        byte f = (byte) twentyFife;
+        int twentyFive = 10000;
+        byte f = (byte) twentyFive;
         System.out.println(f);
     }
 }
