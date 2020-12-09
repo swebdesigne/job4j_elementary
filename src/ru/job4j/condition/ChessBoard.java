4ca79e7eb453b07d4d42e3a4423e4a25fc0677e7
Igor <swebdesigne@gmail.com>
2020-12-09 14:28:17 +0300

    Math.abs

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2){
+        int rsl = 0;
+        if(x1 == x2 || y1 == y2){
+            rsl = Math.abs(x2 - x1);
+            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+        }
+        return rsl;
+    }
+    public static int moveElephant(int x1, int y1, int x2, int y2){
+        int rsl = 0;
+        if(x1 != x2 && y1 != y2){
+            rsl = Math.abs((x2 - x1) + (y2 - y1));
+        }
+        return rsl;
+    }
+    public static void main(String[] args){
+        int rsl = moveElephant(0, 0,5,7);
+        System.out.println(rsl);
+    }
+}
---
+++ test/ru/job4j/condition/ChessBoardTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class ChessBoardTest {
+
+    @Test
+    public void wayIs6() {
+        int way = ChessBoard.way(0,0,0,6);
+        assertThat(way, is(6));
+    }
+
+    @Test
+    public void wayIs5(){
+        int way = ChessBoard.way(0, 0,5,0);
+        assertThat(way,is(5));
+    }
+
+    @Test
+    public void wayIs0(){
+        int way = ChessBoard.way(0,0,5,6);
+        assertThat(way, is(0));
+    }
+
+    @Test
+    public void moveElephantIs0(){
+        int way = ChessBoard.moveElephant(0,0,5,0);
+        assertThat(way, is(0));
+    }
+    @Test
+    public void moveElephantIs12(){
+        int way = ChessBoard.way(0, 0,5,7);
+        assertThat(way, is(12));
+    }
+}
