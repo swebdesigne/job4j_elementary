9a756665039289e6fc5b28a16a6c1d76426faae8
swebdesigne <swebdesigne@gmail.com>
2020-12-09 21:22:20 +0300

    Math.abs

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -13,7 +13,7 @@ public class ChessBoard {
 
     public static int moveElephant(int x1, int y1, int x2, int y2){
         int rsl = 0;
-        if(x1 - x2 == y1 - y2 || x1 - x2 == y2 - y1){
+        if(Math.abs(x2 - x1) == Math.abs(y2 - y1)){
             rsl = Math.abs(x2 - x1) + Math.abs(y2 - y1);
         }
         return rsl;
