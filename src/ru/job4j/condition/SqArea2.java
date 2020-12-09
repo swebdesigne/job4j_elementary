5ecd775be0c1eaa71f472a4d1ecb53596e375002
swebdesigne <swebdesigne@gmail.com>
2020-12-09 21:15:55 +0300

    SqArea2

---
+++ src/ru/job4j/condition/SqArea2.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class SqArea2 {
+    public static int square(int p, int k){
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
+    }
+}
