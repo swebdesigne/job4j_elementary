3203bf0aeb8037b982ed0c80fd790417d08ddf4e
swebdesigne <swebdesigne@gmail.com>
2020-12-11 21:16:24 +0300

    Подсчет суммы чётных чисел в диапазоне.

---
+++ src/ru/job4j/Test.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Test {
+    public static void main(String[] args){
+        int i, j;
+        boolean isprime;
+        for (i = 2; i < 100; i++){
+            isprime = true;
+            for(j = 2; j < i / j; j++){
+                if((i % j) == 0) isprime = false;
+                if(isprime)
+                    System.out.println(i + " - простое число");
+            }
+        }
+    }
+}
---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class CounterTest {
+
+    @Test
+    public void whenS0ToF10Then55() {
+        int rsl = Counter.sumByEven(0,10);
+        assertThat(rsl, is(30));
+    }
+
+    @Test
+    public void whenS3ToF8Then33() {
+        int rsl = Counter.sumByEven(3,8);
+        assertThat(rsl, is(18));
+    }
+
+    @Test
+    public void whenS1ToF1Then1() {
+        int rsl = Counter.sumByEven(1,1);
+        assertThat(rsl, is(0));
+    }
+}
