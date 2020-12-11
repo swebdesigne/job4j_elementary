1db824c688bd56144f048921ded14b9ff2d677e8
swebdesigne <swebdesigne@gmail.com>
2020-12-11 21:39:06 +0300

    Создать программу, вычисляющую факториал.

---
+++ src/ru/job4j/loop/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n){
+        int sum = 1;
+        for(int index = 0; index <= n; index++){
+            sum *= index <= 0 ? 1 : index;
+        }
+        return sum;
+    }
+}
