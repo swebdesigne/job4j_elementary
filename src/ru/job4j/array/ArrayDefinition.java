6d448d3b6d0e0c4dbbdeb362b0f43d0ddd127d19
Igor <swebdesigne@gmail.com>
2020-12-15 11:25:01 +0300

    Заполнение массива.

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен:"  + surname.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] name = new String[4];
+        name[0] = "Igor";
+        name[1] = "Boris";
+        name[2] = "Alina";
+        name[3] = "Fedor";
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+        }
     }
 }
