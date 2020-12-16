84b655fc1a36682487d58a1f9875005b6ba95163
Igor <swebdesigne@gmail.com>
2020-12-16 20:45:23 +0300

    Перевернуть массив.

---
+++ test/ru/job4j/array/TurnTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2, 5};
+        int[] result = Turn.back(input);
+        int[] expect = new int[] {5, 2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+}
