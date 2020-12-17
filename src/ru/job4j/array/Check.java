a21d8aaf67d7171392220aee31eb25227fcc14a5
Igor <swebdesigne@gmail.com>
2020-12-17 21:05:56 +0300

    Массив заполнен true или false

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 0; index < data.length; index++) {
+            if (index + 1 < data.length && data[index] != data[index + 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ test/ru/job4j/array/CheckTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class CheckTest {
+
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        boolean[] input = new boolean[] {true, true, true};
+        boolean rsl = Check.mono(input);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void whenDataNoMonoByTrueThenFalse() {
+        boolean[] input = new boolean[] {true, false, true};
+        boolean rsl = Check.mono(input);
+        assertThat(rsl, is(false));
+    }
+}
