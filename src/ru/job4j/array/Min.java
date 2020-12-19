cca9456d45fedb52530fb153166ea813e1898089
Igor <swebdesigne@gmail.com>
2020-12-19 13:38:09 +0300

    Поиск минимального числа в массиве

---
+++ src/ru/job4j/array/Min.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            min = min > array[i] ? array[i] : min;
+        }
+        return min;
+    }
+}
