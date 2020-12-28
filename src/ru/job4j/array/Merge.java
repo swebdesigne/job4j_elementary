8fd249decf4b02774b400ddf19752cf18b28126c
Igor <swebdesigne@gmail.com>
2020-12-28 10:56:14 +0300

    Объединить два отсортированных массива

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -4,29 +4,28 @@ public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int iterator = 0;
+        int leftCount = 0;
+        int rightCount = 0;
         for (int i = 0; i < rsl.length; i++) {
-            int leftIterator = i < left.length ? left[i] : 0;
-            int rightIterator = i < right.length ? right[i] : 0;
-            if (leftIterator < rightIterator) {
-                rsl[iterator] = leftIterator;
-                if (left[i] < rightIterator) {
-                    rsl[iterator + 1] = rightIterator;
+            if (leftCount < left.length && rightCount < right.length) {
+                if (left[leftCount] < right[rightCount]) {
+                    rsl[iterator] = left[leftCount];
+                    leftCount++;
+                } else {
+                    rsl[iterator] = right[rightCount];
+                    rightCount++;
                 }
-            }
-            iterator += 2;
-
-//            System.out.println(rsl[i]);
+                iterator++;
+            } else if (leftCount != left.length) {
+               rsl[iterator] = left[leftCount];
+               iterator++;
+               leftCount++;
+           } else if (rightCount != right.length) {
+               rsl[iterator] = right[rightCount];
+               iterator++;
+               rightCount++;
+           }
         }
-
         return rsl;
-    }
-
-    public static void main(String[] args) {
-        int[] left = new int[] {1, 2};
-        int[] right = new int[] {3, 4};
-        int[] rsl = merge(left, right);
-        for (int r: rsl) {
-            System.out.println(r);
-        }
     }
 }
