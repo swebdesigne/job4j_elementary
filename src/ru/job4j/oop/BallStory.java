3839b45229875be8106523bbf58eef1c059fc779
Igor <swebdesigne@gmail.com>
2020-12-30 09:25:55 +0300

    Взаимодействие объектов

---
+++ src/ru/job4j/oop/BallStory.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare =  new Hare();
+        Fox fox = new Fox();
+        Wolf wolf = new Wolf();
+        ball.tryEat(ball);
+        hare.tryEat(ball);
+        fox.tryEat(ball);
+        wolf.tryEat(ball);
+    }
+}
