2b736060d95aa61e69862ea2cfa05aa7c0bc05bb
Igor <swebdesigne@gmail.com>
2021-01-03 13:28:24 +0300

    Calculator

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,15 +1,20 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-   public  static void plus(int first, int second) {
-       int result = first + second;
-       System.out.println(result);
+   public double add(double first, double second) {
+       return first + second;
    }
 
-   public static  void main(String[] args) {
-       Calculator.plus(1, 2);
-       Calculator.plus(10, 11);
+   public double add(double first, double second, double third) {
+       return add(
+               first, add(second, third)
+       );
    }
+
+    public static void main(String[] args) {
+        Calculator rsl = new Calculator();
+        System.out.println(rsl.add(1, 1, 5));
+    }
 }
 
 
