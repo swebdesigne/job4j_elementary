d5f7478eab76acc1718101c7183777ef3bd97089
Igor <swebdesigne@gmail.com>
2021-01-10 17:45:15 +0300

    Police

---
+++ src/ru/job4j/pojo/Police.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Police {
+    public static void main(String[] args) {
+        License license = new License();
+        license.setOwner("Igor Sivolobov");
+        license.setModel("Kia");
+        license.setCode("xx111x");
+        license.setCreated(new Date());
+
+        System.out.println(license.getOwner() + " has a car - " + license.getModel() + " : " + license.getCode());
+    }
+}
