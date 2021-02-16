3a75919edc88a2147b9f888406b76f05c40ee3db
Vanquish101101 <sergantforce@ya.ru>
2021-02-16 21:31:49 +0300

    decorator1

--- src/ru/job4j/my/decorator/Family.java
+++ src/ru/job4j/my/decorator/Family.java
@@ -2,7 +2,7 @@ package ru.job4j.my.decorator;
 
 public class Family {
     public static void main(String[] args) {
-        OurFemily ourFemily = new Mather(new Father(new Son()));
+        OurFemily ourFemily = new Father(new Mather(new Son()));
         System.out.println(ourFemily.make());
     }
 }
