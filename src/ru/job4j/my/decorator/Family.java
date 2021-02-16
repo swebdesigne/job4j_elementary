ca42021663bca7fe1abf03f31422f7421a849c90
Igor <swebdesigne@gmail.com>
2021-02-16 21:46:35 +0300

    decorator

--- src/ru/job4j/my/decorator/Family.java
+++ src/ru/job4j/my/decorator/Family.java
@@ -1,10 +1,8 @@
 package ru.job4j.my.decorator;
 
 public class Family {
-
     public static void main(String[] args) {
         OurFemily ourFemily = new Father(new Mather(new Son()));
         System.out.println(ourFemily.make());
     }
-
 }
